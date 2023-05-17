1d7b32cfd1582526b680f2f07d9aedb73ead1a4e
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-17 15:01:34 -0300

    featconfiguring external sources

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,31 @@
 // eslint-disable-next-line prettier/prettier
+
 import { StatusBar } from "expo-status-bar";
-// eslint-disable-next-line prettier/prettier
+
 import { Text, View } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
+    <View className="flex-1 items-center justify-center bg-gray-900">
       <Text className="text-5xl font-bold text-zinc-50">I love you baby</Text>
       <StatusBar style="auto" />
     </View>
-    // eslint-disable-next-line prettier/prettier
   );
 }
