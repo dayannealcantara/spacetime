a6911717e1530677fb831b31d2925e255994613f
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-16 21:29:54 -0300

    feat:configuring external source

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto'})
+const baiJamjure = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree'
+})
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} ${baiJamjure.variable} font-sans`}>{children}</body>
     </html>
   )
 }
