e6a3d5f910ff4198089ce5e016ef456f68ca64e0
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-17 20:45:03 -0300

    feat: installing and configuring cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 /* eslint-disable prettier/prettier */
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true
+})
 
 app.register(memoriesRoutes)
 
