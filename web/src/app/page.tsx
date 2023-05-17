b1e85544d1e18cf7f38e8c90e497315a2f63d61b
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-17 14:18:22 -0300

    feat: developing the login session

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { User } from "lucide-react";
 
 
 export default function Home() {
@@ -6,9 +7,19 @@ export default function Home() {
     <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         {/* Blur */}
       <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
-
        {/* Stripes */}
       <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+      <a 
+        href="" 
+        className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+      >
+        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-slate-400">
+          <User className="h-5 w-5 text-gray-500"/>
+        </div>
+        <p className="max-w-[140px] text-sm leading-snug">
+          <span className="underline">Crie sua conta</span> e salve memórias
+        </p>
+      </a>
 
     </div>
     <div className='flex flex-col p-16'>
