db6f004b3d4794bb78a689415ab94c5a66e4cd32
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-18 15:12:43 -0300

    feat: creating Jwt access token

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 /* eslint-disable prettier/prettier */
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
@@ -10,7 +14,12 @@ app.register(cors, {
   origin: true
 })
 
+app.register(jwt, {
+  secret: 'spacetime'
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
